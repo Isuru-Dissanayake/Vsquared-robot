bb195429afef4c0d2a81224381ffc3c8e397b888
Isuru Dissanayake <isurumailme@gmail.com>
2019-05-29 23:04:24 +0530

    Add files via upload

---
+++ h_encoders.ino
@@ -0,0 +1,34 @@
+
+void Turn_Leftninety()
+{
+  
+  while (Leften_Count <= 50)
+  {
+    Turn_Left();
+    Serial.println(Leften_Count);
+  }
+  
+  Brake();
+  delay(3000);
+  Leften_Count = 0;
+}
+
+void Left_Turns()
+{
+  /*
+  if (digitalRead(Leften_A) && !digitalRead(Leften_B)) {
+    
+    Leften_Count++ ;
+  }
+  if (digitalRead(Leften_A) && digitalRead(Leften_B)) {
+    Left_Count-- ;
+  } 
+  */
+  Leften_Count = Leften_Count + 1;
+}
+void Count()
+{
+  Leften_Count = Leften_Count + 1;
+  //Serial.println(Leften_Count);
+}
+
