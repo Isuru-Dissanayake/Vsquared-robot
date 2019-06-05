2c8e56f919977404d710485f202646c8dcbfc3fb
Isuru Dissanayake <isurumailme@gmail.com>
2019-06-05 20:00:53 +0530

    Add files via upload

---
+++ codes/i_gateServo.ino
@@ -0,0 +1,12 @@
+void openGate() 
+{
+  for (gatePos = 0; gatePos <= 60; gatePos += 5) { 
+    gateServo.write(gatePos);                                     
+  }
+  delay(1000);
+  for (gatePos = 60; gatePos >= 0; gatePos -= 5) {
+    gateServo.write(gatePos);                                    
+  }
+  delay(1000);
+  
+}
